f0e66c6647e714a2ca9e17ebb79398ec2e00ea89
sabhashanki <sabhashanki@gmail.com>
2022-09-21 21:46:10 +0530

    pickle file

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+import requests
+
+app = Flask(__name__)
+
+model = pickle.load(open('credit_default.pkl','rb'))
+
+@app.route('/', method = ['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', method = ['POST'])
+def predict():
+    limit_bal = int(request.form['LIMIT_BAL'])
+    sex = request.form(['sex'])
+    age = request.form(['age'])
+    education = request.form(['education'])
+    marriage = request.form(['marriage'])
+    pay = request.form(['repayment_status'])
+    bill_amt = request.form(['average_bill'])
+    pay_amt1 = request.form(['jan_payment'])
+    pay_amt2 = request.form(['feb_payment'])
+    pay_amt3 = request.form(['march_payment'])
+    pay_amt4 = request.form(['april_payment'])
+    pay_amt5 = request.form(['may_payment'])
+    pay_amt6 = request.form(['june_payment'])
+    predict = 
+
+
