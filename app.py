de21e64a33cf8001b0a841ca2f6190a1b247b0c0
sabhashanki <sabhashanki@gmail.com>
2022-09-22 11:01:22 +0530

    html file completed

--- app.py
+++ app.py
@@ -14,23 +14,55 @@ def home():
 @app.route('/predict', method = ['POST'])
 def predict():
     acc_no = request.form(['acc_no'])
-    limit_bal = int(request.form['limit_bal'])
+    limit_bal = float(request.form['limit_bal'])
     sex = request.form(['sex'])
-    age = request.form(['age'])
+    age = int(request.form(['age']))
     education = request.form(['education'])
     marriage = request.form(['marriage'])
-    pay = request.form(['repayment_status'])
-    bill_amt = request.form(['average_bill'])
-    pay_amt1 = request.form(['jan_payment'])
-    pay_amt2 = request.form(['feb_payment'])
-    pay_amt3 = request.form(['march_payment'])
-    pay_amt4 = request.form(['april_payment'])
-    pay_amt5 = request.form(['may_payment'])
-    pay_amt6 = request.form(['june_payment'])
+    pay = float(request.form(['payment_history']))
+    bill_amt = float(request.form(['average_bill']))
+    pay_amt1 = float(request.form(['jan_payment']))
+    pay_amt2 = float(request.form(['feb_payment']))
+    pay_amt3 = float(request.form(['march_payment']))
+    pay_amt4 = float(request.form(['april_payment']))
+    pay_amt5 = float(request.form(['may_payment']))
+    pay_amt6 = float(request.form(['june_payment']))
+
+    if sex == 'male':
+        sex = 1
+    else:
+        sex = 2
+
+    if education == 'uneducated':
+        education = 0
+    elif education == 'middle_school':
+        education = 1
+    elif education == 'high_school':
+        education = 2
+    elif education == 'ug':
+        education = 3
+    elif education == 'pg':
+        education = 4
+    elif education == 'phd':
+        education = 5
+    else:
+        education = 6
+
+    if marriage == 'unmarried':
+        marriage = 0
+    elif marriage == 'married':
+        marriage = 1
+    elif marriage == 'divorced':
+        marriage = 2
+    else:
+        marriage = 3
+
+
+    
     predict = model.predict([[limit_bal, sex, education, marriage, age
                 , pay, bill_amt, pay_amt1, pay_amt2, pay_amt3, pay_amt4
                 , pay_amt5, pay_amt6]])
-    return render_template('home.html', prediction_text = 'Credit Card Default Status : {}'.format(predict))
+    return render_template('home.html', prediction_text = 'Credit Card Default Status for account {} : {}'.format(acc_no, predict))
 
 
 if __name__ =+ '__main__':
