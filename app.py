2e38b5d06d63782240ce5dbd95b138115fad660d
sabhashanki <sabhashanki@gmail.com>
2022-09-24 15:46:53 +0530

    app file optimized

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, render_template
-import requests
 import pickle
 
 app = Flask(__name__)
 
+#Import pickle model file 
 model = pickle.load(open('credit_default.pkl','rb'))
 
+# Home Page for the flask app
 @app.route('/', methods = ['GET'])
 def home():
     return render_template('home.html')
 
+# Predict function to gather all inputs and predict the outcome
 @app.route('/predict', methods = ['POST'])
 def predict():
 
@@ -28,67 +30,75 @@ def predict():
     pay_amt5 = float(request.form['may_payment'])
     pay_amt6 = float(request.form['june_payment'])
 
+# Module to convert categorical sex values to numerical values
     if sex == 'male':
         sex = 1
     else:
         sex = 2
 
-    if education == 'uneducated':
-        education = 0
-    elif education == 'middle_school':
-        education = 1
-    elif education == 'high_school':
-        education = 2
-    elif education == 'ug':
-        education = 3
-    elif education == 'pg':
-        education = 4
-    elif education == 'phd':
-        education = 5
-    else:
-        education = 6
+# Education Module to convert categorical values into numerical
+    education_dict = {
+        'uneducated' : 0,
+        'middle_school' : 1,
+        'high_school' : 2,
+        'ug' : 3,
+        'pg' : 4,
+        'phd' : 5,
+        'others' : 6,
+        }
 
-    if marriage == 'unmarried':
-        marriage = 0
-    elif marriage == 'married':
-        marriage = 1
-    elif marriage == 'divorced':
-        marriage = 2
-    else:
-        marriage = 3
+    for name, value in education_dict.items():
+        if education == name:
+            education = value
+            break
 
-    if pay == 'on_time':
-        pay = 0
-    elif pay == 'advance_one':
-        pay = -1
-    elif pay == 'advance_two':
-        pay = -2
-    elif pay == 'delay_one':
-        pay = 1
-    elif pay == 'delay_two':
-        pay = 2
-    elif pay == 'delay_three':
-        pay = 3
-    elif pay == 'delay_four':
-        pay = 4
-    elif pay == 'delay_five':
-        pay = 5
-    elif pay == 'delay_six':
-        pay = 6
-    else:
-        pay = 7
+# Marriage module to assign the numrical value for categorical inputs
+    marriage_dict = {
+        'unmarried' : 0,
+        'married' : 1,
+        'divorced' : 2,
+        'widowed' : 3
+        }
 
-    
+    for name, value in marriage_dict.items():
+        if marriage == name:
+            marriage = value
+            break
+
+# Payment module to assign the numrical value for categorical inputs 
+    pay_dict = {
+        'on_time' : 0,
+        'advance_one' : -1,
+        'advance_two' : -2,
+        'delay_one' : 1,
+        'delay_two' : 2,
+        'delay_three' : 3,
+        'delay_four' : 4,
+        'delay_five' : 5,
+        'delay_six' : 6,
+        'delay_seven' : 7
+        }
+
+    for name, value in pay_dict.items():
+        if pay == name:
+            pay = value
+            break
+
+# Module to predict the outcome using ML algorithm
     predict = model.predict([[limit_bal, sex, education, marriage, age
                 , pay, bill_amt, pay_amt1, pay_amt2, pay_amt3, pay_amt4
                 , pay_amt5, pay_amt6]])
+
+# Converting the categorical values into intergers
     if predict == 0:
         output = 'Not Default'
     else:
         output = 'Default'
+
     return render_template('home.html', prediction_text = 'Credit Card Default Status for account {} : {}'.format(acc_no, output))
 
 
+# Driver Code
 if __name__ == '__main__':
     app.run(debug = True)
 
--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+education_dict = {
+       'uneducated' : 0,
+       'middle_school' : 1,
+       'high_school' : 2,
+       'ug' : 3,
+       'pg' : 4,
+       'phd' : 5,
+       'others' : 6,
+    }
 
+education = 'others'
+for name, value in education_dict.items():
+    if education == name:
+        education = value
+        break
 
-from sklearn.pipeline import Pipeline
+print(education)
